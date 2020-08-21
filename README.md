b145ac2f5c61646cac6989952bb14db186e41e25
Eddie Villarreal <eddie.villarreal@outlook.com>
2020-08-21 14:04:41 -0500

    finished color change and updated read me

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # hw5_daily_planner
 Making a daily planner.  
-code
+
+Created a planner that uses moment.js to keep track of time, and local.Storage to save the event.
+
+I used jQuery to get creat elements for the different variables.  One in particular for the times.  Used a for loop to cycle through numbers and create elements for each.
+
+I added elements for decription and the save btn and used the bootstrap grid method to frame it.  
+
+wrote a simple if else statement to link to corresponding css property to change the color of the present, past, and future time blocks.  
+
+Using parent methods, I grabbed what text from the description element, and saved to local.storage.  I used get.storage to fill the description element if something had been saved, and an or operator distinguish whether or not there was text there or to leave it blank.
+
+
--- script.js
+++ script.js
@@ -1,36 +1,41 @@
 $(document).ready(function () {
 
-// I need a var for the times in the planner
-// I need need a var for moment.js
+    // I need a var for the times in the planner
+    // I need need a var for moment.js
 
-    var times = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm" ];
-    var mom = moment();
-    var date = mom.format("dddd MMM Do YYYY")
+    let times = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm" ];
+    let mom = moment();
+    let date = mom.format("dddd MMM Do YYYY")
     console.log(times)
     
     
     $("#currentDay").text(date);
     $("#currentDay").css({"font-size":"24px", "font-weight":"bold"});
    
-// create an element for the planner itself, time, input area, and save button     
+    // create an element for the planner itself, time, input area, and save button     
+   
     
     
+    for(i = 0; i< times.length; i++) { 
     
-    for(var i = 0; i< times.length; i++) { 
+        let newEl = $("<div>");
+        let timeEl = $("<div>");
+        let desEl = $("<textArea>");
+        let btnEl = $("<button>");
+        let iconEl = $("<i>");
     
-        var newEl = $("<div>");
-        var timeEl = $("<div>");
-        var desEl = $("<textArea>");
-        var btnEl = $("<button>");
-        var iconEl = $("<i>");
-    
-
+        //create class attributes for elements to get a handle on them
         newEl.attr("data-hour", times[i]);
         newEl.attr("class", "row");
         timeEl.attr("class", "time-Block col-1");
+        
+        // set text for input area to local storage
+        // added the or operator to set the value to the local storage or empty
         desEl.attr("class", "description col-10");
         const value = localStorage.getItem(times[i]) || "";
         desEl.val(value);
+        
+        //need to pull the image for the btn
         btnEl.attr("class", "saveBtn col-1");
         iconEl.attr("class", "far fa-save");
         
@@ -41,15 +46,44 @@ $(document).ready(function () {
         btnEl.append(iconEl);
         timeEl.text(times[i]);
         
+        //if statement for colorchange?
+        //since moment is in military time, need to add 9 to i to bring it to 9 and so on
+        //simple if else statements adding class attr for css property 
+        if((i+9) < moment().hour()){
+            desEl.attr("class", "past col-10 description");
+        }else if((i+9) === moment().hour()){
+            desEl.attr("class", "present col-10 description");
+        }else{
+            desEl.attr("class", "future col-10 description");
+        }
         
+        
+        
+        
+        
+        
+        //click function to set the value of the desciption to localStorage
+        //used .parent and .find to locate .description in it's parent
+        //ued .parent.attr to locate the attribute of the element and use said element as the key
 
         btnEl.click(function() {
-            var data = $(this).parent().find(".description").val()
-            var key = $(this).parent().attr("data-hour")
+            let data = $(this).parent().find(".description").val()
+            let key = $(this).parent().attr("data-hour")
             localStorage.setItem(key, data);
     
         })
       
     }
-    localStorage.getItem("data")
+    
 })
+
+
+
+
+
+
+
+
+
+
+
